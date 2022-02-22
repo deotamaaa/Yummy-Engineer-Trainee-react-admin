7d2a713272f2fc8fe604fe61fc2acfc1568799b8
Deotama <deotama11@gmail.com>
2022-02-22 15:00:15 +0700

    Video 45 Done. Added User name.

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,22 +1,32 @@
-import { Component } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-class Nav extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-                <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="{#}">Company name</a>
-                <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
-                <ul className="navbar-nav px-3">
-                    <li className="nav-item text-nowrap">
-                        <a className="nav-link" href="{#}">Sign out</a>
-                    </li>
-                </ul>
-            </nav>
-        )
-    }
+const Nav = () => {
+
+    const [user, setUser] = useState({
+        firstName: '',
+    });
+
+
+    useEffect(() => {
+        (async () => {
+            const { data } = await axios.get('http://localhost:8000/api/user', { withCredentials: true });
+
+            setUser(data);
+        })();
+    }, []);
+
+    return (
+        <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+            <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="{#}">Company name</a>
+            <ul className="navbar-nav px-3">
+                <li className="nav-item text-nowrap">
+                    <a className="nav-link text-white text-decoration-none" href="{#}">{user.firstName}</a>
+                    <a className="nav-link" href="{#}">Sign out</a>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Nav;
